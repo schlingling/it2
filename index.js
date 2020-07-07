6e99a0ba6724f3ce210e3793b01a3ca6f435ae34
schlingling <58326464+schlingling@users.noreply.github.com>
2020-07-07 16:01:40 +0200

    foreach implementiert

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
+var rawData = []
+
+window.onload = initialisiere();
+
+function initialisiere() {
+    rawData.push(d3.json("http://it2wi1.if-lab.de/rest/ft_ablauf"))
+}
+
+Promise.all(rawData).then((result) => preprocess_rawData(result), function (error) {
+    console.log(error);
+});
+
+
+
+function preprocess_rawData (rawData, error){
+    if (error) {
+        console.log(error);
+    } else {
+       console.log(rawData);
+
+       rawData[0].forEach(zeitpunkt => {
+           for (const [key, val] of Object.entries(zeitpunkt.werte)){
+               console.log(key,val);
+           }
+       });
+
+    
+    }
+
+}
+
+
+
+
+
+
+
+/*
+
+
 function aktualisiere() {
     //Daten empfangen
     d3.json("http://it2wi1.if-lab.de/rest/beispiel/Parameter/Person%20B/tag/1").then(function (data, error) {
@@ -10,7 +50,7 @@ function empfangeDaten(datenEmpfangen, error) {
     if (error) {
         console.log(error);
     } else {
-        zeigeDaten(datenEmpfangen);
+        raw_data.push(datenEmpfangen);
     }
 }
 
@@ -26,3 +66,5 @@ function zeigeDaten(daten) {
     // Daten löschen, falls es mehr Elemente im HTML als Daten gibt.
     p.exit().remove();
 }
+
+*/
