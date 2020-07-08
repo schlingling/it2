0fcc98918ab0518f49371a237de47f2ff83fd94d
schlingling <58326464+schlingling@users.noreply.github.com>
2020-07-08 22:48:46 +0200

    graphs implemented

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function initialisiere() {
 
 function preprocess_rawData(rawData, error) {
 
-   let valueToComulate = ["H-vertikal", "H-horizontal","V-vertikal","V-drehen", "V-horizontal"];
+    let valueToComulate = ["H-vertikal", "H-horizontal", "V-vertikal", "V-drehen", "V-horizontal"];
 
     let countTaster = {};
     if (error) {
@@ -37,8 +37,8 @@ function preprocess_rawData(rawData, error) {
 
                     if (valueToComulate.includes(key) && (rawData[0][i - 1].werte[key] != val)) {
                         //countTaster
-                       // console.log(val)
-                        countTaster[key] += parseInt(val,10)
+                        // console.log(val)
+                        countTaster[key] += parseInt(val, 10)
 
                     } else if ((rawData[0][i - 1].werte[key]) != val) {
                         countTaster[key] += 1
@@ -51,7 +51,7 @@ function preprocess_rawData(rawData, error) {
 
 
     }
-   // console.log(countTaster),
+    // console.log(countTaster),
     liste = [];
 
     //Listen von Modul Fischertechnik
@@ -59,7 +59,7 @@ function preprocess_rawData(rawData, error) {
     listeVerteilstationFi = ["V-Referenzschalter vertikal", "V-Referenzschalter horizontal", "V-Referenzschalter drehen"]
     listeBearbeitungsstationFi = ["B-Motor Drehkranz im Uhrzeigersinn", "B-Motor Drehkranz gegen Uhrzeigersinn", "B-Motor Foerderband vorwaerts", "B-Motor Saege", "B-Motor Ofenschieber Einfahren", "B-Motor Ofenschieber Ausfahren", "B-Motor Sauger zum Ofen", "B-Motor Sauger zum Drehkranz", "B-Leuchte Ofen", "B-Referenzschalter Drehkranz (Pos. Sauger)", "B-Referenzschalter Drehkranz (Pos. Foerderband)", "B-Lichtschranke Ende Foerderband", "B-Referenzschalter Drehkranz (Pos. Saege)", "B-Referenzschalter Sauger (Pos. Drehkranz)", "B-Referenzschalter Ofenschieber Innen", "B-Referenzschalter Ofenschieber Aussen", "B-Referenzschalter Sauger (Pos. Brennofen)", "B-Lichtschranke Brennofen"]
     listeSortierstationFi = ["S-Lichtschranke Eingang", "S-Lichtschranke nach Farbsensor", "S-Lichtschranke weiss", "S-Lichtschranke rot", "S-Lichtschranke blau", "S-Motor Foerderband"]
-    listeComulate = ["H-vertikal", "H-horizontal","V-vertikal","V-drehen", "V-horizontal"];
+    listeComulate = ["H-vertikal", "H-horizontal", "V-vertikal", "V-drehen", "V-horizontal"];
 
     //Listen von Modul Festo
     listeFesto = ["Umsetzer Endanschlag 1 (3B1)", "Umsetzer Endanschlag 2 (3B2)"]
@@ -72,7 +72,7 @@ function preprocess_rawData(rawData, error) {
     let mapVerteilstationFi = mapModule(listeVerteilstationFi, liste);
     let mapBearbeitungsstationFi = mapModule(listeBearbeitungsstationFi, liste);
     let mapSortierstationFi = mapModule(listeSortierstationFi, liste);
-    let mapComulate = mapModuleComulate(listeComulate,liste); //ACHTUNG: Für Kummulierte Wegstrecke extra Mapmethode!
+    let mapComulate = mapModuleComulate(listeComulate, liste); //ACHTUNG: Für Kummulierte Wegstrecke extra Mapmethode!
 
     let mapFesto = mapModule(listeFesto, liste);
 
@@ -81,26 +81,64 @@ function preprocess_rawData(rawData, error) {
     aktualisiereListe(mapBearbeitungsstationFi, "bearbeitungsstation");
     aktualisiereListe(mapSortierstationFi, "sortierstation");
 
-    
+
     aktualisiereListeComulate(mapComulate, "verteilstation_schwenkarm"); //ACHTUNG: Für Kummulierte Wegstrecke extra aktualisiere()!
 
-    //zeigeDiagram(mapHochregalFi);
+    zeigeDiagram(mapHochregalFi, "hochregallagerG");
+    zeigeDiagram(mapVerteilstationFi, "verteilstationG");
+    zeigeDiagram(mapBearbeitungsstationFi, "bearbeitungsstationG");
+    zeigeDiagram(mapSortierstationFi, "sortierstationG");
+
 
 }
 
 
-function zeigeDiagram(liste) {
+function zeigeDiagram(liste, targetid) {
 
-    let svg = d3.select("#hochregallagerG").append("svg")//Auf Webseite
-        .attr("width", 1000)
-        .attr("height", 400)
-        .style("background-color", "white");
+    id = "#"+targetid;
 
-    var balken = svg.selectAll("rect").data(liste);
-    balken.enter().append("rect")
-        .attr("x", 100);
-    balken.exit().remove();
+    console.log(liste)
+    // set the dimensions and margins of the graph
+    var margin = { top: 20, right: 20, bottom: 30, left: 40 },
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
 
+    // set the ranges
+    var x = d3.scaleBand()
+        .range([0, width])
+        .padding(0.1);
+    var y = d3.scaleLinear()
+        .range([height, 0]);
+
+    let svg = d3.select(id).append("svg")//Auf Webseite
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+    // Scale the range of the data in the domains
+    x.domain(liste.map(function (d) { return d.key; }));
+    y.domain([0, d3.max(liste, function (d) { return d.val; })]);
+
+    // append the rectangles for the bar chart
+    svg.selectAll(".bar")
+        .data(liste)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function (d) { return x(d.key); })
+        .attr("width", x.bandwidth())
+        .attr("y", function (d) { return y(d.val); })
+        .attr("height", function (d) { return height - y(d.val); });
+
+    // add the x Axis
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
+
+    // add the y Axis
+    svg.append("g")
+        .call(d3.axisLeft(y));
 
 }
 
@@ -115,12 +153,12 @@ function mapModule(listeModul, listeGlobal) {
     return filteredListe;
 }
 
-function mapModuleComulate(listeModul,listeGlobal){
+function mapModuleComulate(listeModul, listeGlobal) {
     let filteredListe = listeGlobal.map(function (d, i) {
         if (listeModul.includes(d.key)) {
             //console.log(d)
             //console.log(d.val)
-            d.val = Math.round(d.val/79)
+            d.val = Math.round(d.val / 79)
             return (d);
         }
     }).filter(function (x) {
